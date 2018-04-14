99e3317c0599373147082352b701b478ab0fab42
Zack Penka <zpenka@gmail.com>
2018-04-14 14:11:11 -0600

    Add bird handlers
    * Add markup
    * Add form to post new birds

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 func newRouter() *mux.Router {
   r:= mux.NewRouter()
   r.HandleFunc("/hello", handler).Methods("GET")
+  r.HandleFunc("/bird", getBirdHandler).Methods("GET")
+  r.HandleFunc("/bird", createBirdHandler).Methods("POST")
 
   staticFileDirectory := http.Dir("./assets/")
   staticFileHandler := http.StripPrefix("/assets/", http.FileServer(staticFileDirectory))
