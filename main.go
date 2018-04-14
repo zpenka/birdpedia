2c11946d0b0915d3a6bf043c9017af668665158c
Zack Penka <zpenka@gmail.com>
2018-04-14 13:03:46 -0600

    Add routing

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
   "fmt"
   "net/http"
+
+  "github.com/gorilla/mux"
 )
 
 func main() {
-  http.HandleFunc("/", handler)
+  // Declare router
+  r := mux.NewRouter()
 
-  http.ListenAndServe(":8080", nil)
+  r.HandleFunc("/hello", handler).Methods("GET")
+
+  http.ListenAndServe(":8080", r)
 }
 
 func handler (w http.ResponseWriter, r *http.Request) {
